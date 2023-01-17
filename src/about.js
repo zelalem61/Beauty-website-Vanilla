4b0f89cacf61fedf1ceda640d0133629984d310e
zelalem61 <108578495+zelalem61@users.noreply.github.com>
2023-01-17 12:31:38 -0800

    Delete about.js

--- src/about.js
+++
@@ -1,8 +0,0 @@
-
-function showMore(){
-    const text = document.querySelector(".about-main__not-displayed");
-    const btn = document.querySelector(".read-more");
-    text.setAttribute("style", "display: block;");
-    btn.style.display = "none"
-}
-
